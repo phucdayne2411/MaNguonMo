c5d9627ef0b4d003ecaa15e4d164e3a3e9777b43
phuc <ledanghaiphuc24@gmail.com>
2025-12-16 22:58:55 +0700

    Connect frontend to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,10 @@
 function callAPI() {
-fetch("http://localhost:3000/api/hello")
-.then(res => res.json())
-.then(data => {
-document.getElementById("result").innerText = data.message;
-})
-.catch(err => {
-document.getElementById("result").innerText = "Không gọi được backend";
-});
+  fetch("https://ma-nguon-mo.onrender.com/api/hello")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("result").innerText = data.message;
+    })
+    .catch(() => {
+      document.getElementById("result").innerText = "Backend lỗi ❌";
+    });
 }
